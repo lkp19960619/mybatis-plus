42a86dfc3c1411a97b81bc8ca854a6f9135ad5a7
likaipeng <lkp1107440885@163.com>
2023-03-30 23:55:48 +0800

    源码学习环境搭建

---
+++ mybatis-plus-test/src/main/java/com/baomidou/MybatisApplication.java
@@ -0,0 +1,17 @@
+package com.baomidou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: lkp
+ * @Date: ${DATE} ${TIME}
+ * @Description:
+ * @Version: 2.0
+ */
+@SpringBootApplication
+public class MybatisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+}
